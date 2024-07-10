a5dc465586dcae399f310889e58591b4ec8f4177
naxatra2 <nakshatra28@kgpian.iitkgp.ac.in>
2024-07-11 00:02:07 +0530

    added postgres

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,6 +1,7 @@
-export async function load({ params }) {
-    // const slug= ${params.slug};
-    return {
-        content : `hello ${params.slug }`
-    }    
-}
+import { sql } from "@vercel/postgres";
+
+export async function load({ params, locals }) {
+  return {
+    names: await sql`SELECT * from NAMES where user_id='${locals.user}'`
+  }
+} 
