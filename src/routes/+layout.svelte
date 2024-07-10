f57092ed5c91ab36ab0dcb29e72f2796c9397a4f
naxatra2 <nakshatra28@kgpian.iitkgp.ac.in>
2024-07-10 23:43:27 +0530

    added tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Navbar from '$lib/navbar.svelte';
+    import Footer from '$lib/footer.svelte';
+    import "../app.css"
+</script>
+
+<Navbar />
+
+<slot />
+<Footer />
+
--- src/routes/blog/[slug]/+page.js
+++
@@ -1,6 +0,0 @@
-export function load({ params }) {
-    // const slug= ${params.slug};
-    return {
-        content : "Helloworld"
-    }    
-}
